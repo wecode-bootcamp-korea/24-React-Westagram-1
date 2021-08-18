2e8f1aedab53a5814f41359563a2f59b9c5c650c
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-08-18 17:27:05 +0900

    Add: 피드백 반영 초기세팅완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,32 @@
-import React from "react";
-import{ 
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginDonghee from "./pages/dongheeKim/Login/Login";
+import MainDonghee from "./pages/dongheeKim/Main/Main";
+import LoginSeonghwan from "./pages/seonghwanCho/Login/Login";
+import MainSeonghwan from "./pages/seonghwanCho/Main/Main";
+import LoginSeungchan from "./pages/seungchanKim/Login/Login";
+import MainSeungchan from "./pages/seungchanKim/Main/Main";
+import LoginSonghyun from "./pages/songhyunLee/Login/Login";
+import MainSonghyun from "./pages/songhyunLee/Main/Main";
 
-class Routes extends React.Component{
-    render(){
-        return (
-            <Router>
-                <Switch>
 
-                </Switch>
-            </Router>
-        );
-    }
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login-dong" component={LoginDonghee} />
+          <Route exact path="/main-dong" component={MainDonghee} />
+          <Route exact path="/login-seong" component={LoginSeonghwan} />
+          <Route exact path="/main-seong" component={MainSeonghwan } />
+          <Route exact path="/login-chan" component={LoginSeungchan} />
+          <Route exact path="/main-chan" component={MainSeungchan} />
+          <Route exact path="/login-hyun" component={LoginSonghyun}/>
+          <Route exact path="/main-hyun" component={MainSonghyun} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Routes;
