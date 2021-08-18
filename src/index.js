8e2c8d285ef3c2921a021a95d31d6c51302a772e
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-08-18 17:54:13 +0900

    Add : 피드백 반영 초기세팅 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route } from 'react-router-dom';
 import Routes from './Routes';
 import './styles/reset.scss';
 import './styles/common.scss';
