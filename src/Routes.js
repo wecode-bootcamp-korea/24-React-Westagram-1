eec2fd1e992ee0866af1cf804dea83bd48614a35
김승찬 <kimseungchan@gimseungchan-ui-MacBookAir.local>
2021-08-18 16:18:23 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import{ 
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom'
+
+class Routes extends React.Component{
+    render(){
+        return (
+            <Router>
+                <Switch>
+
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default Routes;
