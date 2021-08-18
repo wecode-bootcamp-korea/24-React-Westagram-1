42af709c4f70fd8db9487527afb84b51e0fc0df1
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-08-18 21:34:03 +0900

    Add : westagram 팀 프로젝트에 기존 프로젝트 추가 - Login 페이지 추가 - Main 페이지 추가 - image 파일 추가 - image 파일 소스  수정 - public/index.html font awesome 링크 추가

--- src/pages/seonghwanCho/Login/Login.js
+++ src/pages/seonghwanCho/Login/Login.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
-function Login() {
-  return <div>Nav bar</div>;
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <div className="contentBox">
+          <header>westagram</header>
+          <article>
+            <div className="input">
+              <div>
+                <input
+                  type="text"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                  className="inputBox"
+                  id="inputEmail"
+                />
+              </div>
+              <div>
+                <input
+                  type="password"
+                  placeholder="비밀번호"
+                  className="inputBox"
+                  id="inputPw"
+                />
+              </div>
+            </div>
+            <div>
+              <Link to="/main-seong">
+                <button className="loginBtn">로그인</button>
+              </Link>
+            </div>
+          </article>
+          <footer>
+            <a href="#">비밀번호를 잊으셨나요?</a>
+          </footer>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
