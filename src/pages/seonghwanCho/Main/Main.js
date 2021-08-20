832030c4198d8bca49d00494a825f2fbda6c9baf
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-08-20 20:32:30 +0900

    Add : main page component 분리 - ArticleContent.js component 추가 - ArticleStory.js component 추가 - SidebarRecomUser component 추가

--- src/pages/seonghwanCho/Main/Main.js
+++ src/pages/seonghwanCho/Main/Main.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Main.scss';
 import Nav from '../../../components/Nav/Nav';
+import ArticleStory from './MainComponent/ArticleStory';
+import ArticleContent from './MainComponent/ArticleContent';
+import SidebarRecomUser from './MainComponent/SidebarRecomUser';
 
 class Main extends Component {
   render() {
@@ -11,205 +14,27 @@ class Main extends Component {
           <div className="article">
             <article>
               <ul className="articleStory">
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
-                <li>
-                  <img src="/images/seonghwanCho/profile.jpeg" alt="wecode" />
-                  <div>wecode</div>
-                </li>
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
+                <ArticleStory />
               </ul>
             </article>
             <article>
-              <div className="articleProfile">
-                <span className="articleUserImage">
-                  <img
-                    src="/images/seonghwanCho/profile.jpeg"
-                    alt="wecode"
-                    id="articleProfileImage"
-                  />
-                </span>
-                <span>
-                  <div className="articleUserName">
-                    <strong>wecode_bootcamp</strong>
-                  </div>
-                  <div className="articleUserNameDiscription">
-                    WeCode-위코드
-                  </div>
-                </span>
-                <span className="articleViewMore">
-                  <i className="fas fa-ellipsis-h"></i>
-                </span>
-              </div>
-              <div>
-                <img
-                  src="/images/seonghwanCho/wecode24_1.jpg"
-                  alt="wecode"
-                  className="articleImage"
-                />
-              </div>
-              <div className="articleMenuBar">
-                <ul className="articleMenuList">
-                  <li>
-                    <i className="far fa-heart"></i>
-                  </li>
-                  <li>
-                    <i className="far fa-comment"></i>
-                  </li>
-                  <li>
-                    <i className="fas fa-location-arrow"></i>
-                  </li>
-                </ul>
-                <ul className="articleMenuList">
-                  <li>
-                    <i className="far fa-bookmark"></i>
-                  </li>
-                </ul>
-              </div>
-              <div className="likeBar">
-                <span>
-                  <img
-                    src="/images/seonghwanCho/profile.jpeg"
-                    alt="wecode"
-                    id="userLikebarImage"
-                  />
-                </span>
-                <span>
-                  <strong>seonghwan</strong>님 <strong>외 34명</strong>이
-                  좋아합니다
-                </span>
-              </div>
-              <div className="descriptionBar">
-                <div>
-                  <p>
-                    <span>wecode_bootcamp</span>"위코드는 단순 교육업체가 아닌
-                    개발자 커뮤니티입니다. Wecode에서 배우고 저는 총 5개
-                    회사에서 오퍼를 받았습니다." - Wecode 졸업생 강병진님...
-                  </p>
-                </div>
-                <div>20분 전</div>
-                <div className="commentWindow">
-                  <p className="commentBox">
-                    <span>
-                      <span className="commentId"></span>
-                      <span className="commentContent"></span>
-                    </span>
-                    <span className="commentLikeBtn"></span>
-                  </p>
-                </div>
-                <div className="commentBar">
-                  <input
-                    type="text"
-                    placeholder="댓글 달기..."
-                    className="commentBarContent"
-                  />
-                  <button className="commentBarPostbtn">게시</button>
-                </div>
-              </div>
+              <ArticleContent />
             </article>
             <article>
-              <div className="articleProfile">
-                <span className="articleUserImage">
-                  <img
-                    src="/images/seonghwanCho/profile.jpeg"
-                    alt="wecode"
-                    id="articleProfileImage"
-                  />
-                </span>
-                <span>
-                  <div className="articleUserName">
-                    <strong>wecode_bootcamp</strong>
-                  </div>
-                  <div className="articleUserNameDiscription">
-                    WeCode-위코드
-                  </div>
-                </span>
-                <span className="articleViewMore">
-                  <i className="fas fa-ellipsis-h"></i>
-                </span>
-              </div>
-              <div>
-                <img
-                  src="/images/seonghwanCho/wecode24_2.jpeg"
-                  alt="wecode"
-                  className="articleImage"
-                />
-              </div>
-              <div className="articleMenuBar">
-                <ul className="articleMenuList">
-                  <li>
-                    <i className="far fa-heart"></i>
-                  </li>
-                  <li>
-                    <i className="far fa-comment"></i>
-                  </li>
-                  <li>
-                    <i className="fas fa-location-arrow"></i>
-                  </li>
-                </ul>
-                <ul className="articleMenuList">
-                  <li>
-                    <i className="far fa-bookmark"></i>
-                  </li>
-                </ul>
-              </div>
-              <div className="likeBar">
-                <span>
-                  <img
-                    src="/images/seonghwanCho/profile.jpeg"
-                    alt="wecode"
-                    id="userLikebarImage"
-                  />
-                </span>
-                <span>
-                  <strong>seonghwan</strong>님 <strong>외 34명</strong>이
-                  좋아합니다
-                </span>
-              </div>
-              <div className="descriptionBar">
-                <div>
-                  <p>
-                    <span>wecode_bootcamp</span>"위코드는 단순 교육업체가 아닌
-                    개발자 커뮤니티입니다. Wecode에서 배우고 저는 총 5개
-                    회사에서 오퍼를 받았습니다." - Wecode 졸업생 강병진님...
-                  </p>
-                </div>
-                <div>20분 전</div>
-                <div className="comment"></div>
-                <div className="commentBar">
-                  <input
-                    type="text"
-                    placeholder="댓글 달기..."
-                    className="commentBarContent"
-                  />
-                  <button className="commentBarPostbtn">게시</button>
-                </div>
-              </div>
+              <ArticleContent />
+            </article>
+            <article>
+              <ArticleContent />
+            </article>
+            <article>
+              <ArticleContent />
             </article>
           </div>
           <aside>
@@ -238,21 +63,11 @@ class Main extends Component {
                     </a>
                   </span>
                 </div>
-                <div className="sideBarRecommenMain">
-                  <div>
-                    <img
-                      src="/images/seonghwanCho/profile.jpeg"
-                      alt="wecode"
-                      id="sideBarRecommenUserImage"
-                    />
-                  </div>
-                  <div className="sideBarRecommenUserProfile">
-                    아이디
-                    <br />
-                    15분전
-                  </div>
-                  <div className="sideBarRecommenUserfollow">팔로우</div>
-                </div>
+                <SidebarRecomUser />
+                <SidebarRecomUser />
+                <SidebarRecomUser />
+                <SidebarRecomUser />
+                <SidebarRecomUser />
               </div>
               <div className="footerLink">
                 <a href="https://about.instagram.com/">소개</a>
