8f037e602f3aa9bb0f5bdc4c7af3d0a1c8458976
choseonghwan91 <82944360+choseonghwan91@users.noreply.github.com>
2021-08-20 10:59:25 +0900

    Add : 공용 nav bar component 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+// 공용
 import React, { Component } from 'react';
 import './Nav.scss';
 
@@ -5,40 +6,32 @@ class Nav extends Component {
   render() {
     return (
       <nav>
-        <div id="nav_in_layout">
+        <div className="nav_in_layout">
           <h1 className="title">Westagram</h1>
           <div id="input_design">
-            <input id="input_search" type="text" />
+            <input id="input_search" type="text" placeholder="검색" />
             <i className="fas fa-search" id="search_icon"></i>
-            <span>검색</span>
           </div>
-          <div id="icon_array">
-            <a href="#">
-              <img
-                alt="location_img"
-                src="/images/seonghwanCho/location.png"
-                className="img_size"
-              />
-            </a>
-            <a href="#">
-              <img
-                alt="heart_img"
-                src="/images/seonghwanCho/heart.png"
-                className="img_size"
-              />
-            </a>
-            <a href="#">
-              <img
-                alt="profile_img"
-                src="/images/seonghwanCho/profile.png"
-                className="img_size"
-              />
-            </a>
-          </div>
+          <ul className="icon_list">
+            <li>
+              <a href="#">
+                <i className="far fa-compass"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="far fa-heart"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="far fa-user"></i>
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
   }
 }
-
 export default Nav;
