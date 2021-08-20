e19b1a89b62326e2fd3fd463dcba5897e60449c0
donghee <dongdh02076@gmail.com>
2021-08-20 20:18:01 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/dongheeKim/Login/Login.js
+++ src/pages/dongheeKim/Login/Login.js
@@ -3,9 +3,26 @@ import { Link, withRouter } from 'react-router-dom';
 import './Login.scss';
 
 class LoginDonghee extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      pw: '',
+    };
+  }
   goToMain = () => {
     this.props.history.push('/main');
   };
+
+  handleIdInput = e => {
+    this.setState({ id: e.target.value });
+  };
+
+  handlePWInput = e => {
+    this.setState({ pw: e.target.value });
+    console.log(this.state);
+  };
+
   render() {
     return (
       <>
@@ -16,11 +33,13 @@ class LoginDonghee extends Component {
               type="text"
               class="id loginInput"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
             />
             <input
               type="password"
               class="password loginInput"
               placeholder="비밀번호"
+              onChange={this.handlePWInput}
             />
             <button onClick={this.goToMain} class="loginBtn">
               로그인
