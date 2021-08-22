4ecbf0a6328d4f6281d0313c7fbdd0599fd936a7
vi2920va <vi2920va@gmail.com>
2021-08-22 10:24:32 +0900

    Add: 로그인 레이아웃 추가  - 로그인 컴포넌트 레이아웃 완성

--- src/pages/songhyunLee/Login/Login.js
+++ src/pages/songhyunLee/Login/Login.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Login.scss';
 
-function Login() {
-  return <div>Nav bar</div>;
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      pwd: '',
+      pass: false,
+    };
+  }
+
+  handleChange(e) {
+    if (e.target.value.length > 1 && e.target.value.includes('@')) {
+      this.setState({
+        email: e.target.value,
+      });
+    }
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="login">
+        <div className="login-container">
+          <h2 className="westagram-logo">westagram </h2>
+          <form className="login-form">
+            <input
+              type="email"
+              className="login-input"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={e => this.handleChange(e)}
+              aria-label="이메일"
+            />
+            <input
+              type="password"
+              className="login-input"
+              placeholder="비밀번호"
+              autoComplete="off"
+              aria-label="비밀번호"
+            />
+            <button type="submit" className="login-btn">
+              로그인
+            </button>
+          </form>
+          <p className="singUp">
+            <span>비밀번호 찾기</span> / <a href="/signup">가입하기</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
