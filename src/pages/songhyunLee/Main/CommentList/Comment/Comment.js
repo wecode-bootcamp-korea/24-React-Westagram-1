9d6de1480eecfb3e0424b1e174f58f54c76a8e44
vi2920va <vi2920va@gmail.com>
2021-08-23 15:20:52 +0900

    Add: 댓글 리스트 컴포넌트 레이아웃

---
+++ src/pages/songhyunLee/Main/CommentList/Comment/Comment.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return <div>Hi</div>;
+  }
+}
+
+export default Comment;
---
+++ src/pages/songhyunLee/Main/CommentList/CommentList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Comment from './Comment/Comment';
+import './CommentList.scss';
+
+class CommentList extends Component {
+  render() {
+    return (
+      <>
+        <h3>Come on</h3>
+      </>
+    );
+  }
+}
+
+export default CommentList;
