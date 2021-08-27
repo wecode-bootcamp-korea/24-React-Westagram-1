cab58c158d1c223219495c01f22db1488d7bdaeb
donghee <dongdh02076@gmail.com>
2021-08-27 09:16:57 +0900

    FIX : 메인페이지 불필요한 주석 삭제

--- src/pages/dongheeKim/Main/Main.js
+++ src/pages/dongheeKim/Main/Main.js
@@ -23,32 +23,6 @@ class MainDonghee extends Component {
       });
   }
 
-  // getComment = event => {
-  //   this.setState({
-  //     value: event.target.value,
-  //   });
-  // };
-
-  // createComment = e => {
-  //   const comment = {
-  //     idx: Date.now(),
-  //     accountName: 'donghee',
-  //     commentInput: this.state.value,
-  //   };
-
-  //   this.setState({
-  //     commentList: this.state.commentList.concat(comment),
-  //   });
-  //   var input = document.getElementsByClassName('feeds-comment_box')[0];
-  //   input.value = null;
-  // };
-
-  // createCommentEnter = e => {
-  //   if (e.key === 'Enter') {
-  //     this.createComment();
-  //   }
-  // };
-
   render() {
     return (
       <div className="MainDonghee">
